f534adab02482dcddc981e2b3d3603ccf740249e
mzwandile.mahlangu <mzwandile.mahlangu@entelect.co.za>
2017-06-17 18:21:57 +0200

    Working on functionality to click and drag over multiple TextViews

--- app/src/main/java/za/co/mzwandile/finlitapp_crossword_puzzle/MainActivity.java
+++ app/src/main/java/za/co/mzwandile/finlitapp_crossword_puzzle/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,10 +20,113 @@ public class MainActivity extends AppCompatActivity {
     private String constructedWord = "";
     private int correctWordTextViewID = 0;
     private List<TextView> selectedTextViews = new LinkedList<>();
+    private List<TextView> puzzleBlocks = new LinkedList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setOnTouchListenerOnLayout();
+        setOnTouchListenerOnButon();
+    }
+
+    private void setOnTouchListenerOnButon(){
+        final int ROWS = 4;
+        final int COLS = 5;
+        final int SIZE = 4;
+        final LinearLayout puzzleBoxLayout[] = new LinearLayout[SIZE];
+
+        puzzleBoxLayout[0] = (LinearLayout) findViewById(R.id.puzzle_box_layout_0);
+        puzzleBoxLayout[1] = (LinearLayout) findViewById(R.id.puzzle_box_layout_1);
+        puzzleBoxLayout[2] = (LinearLayout) findViewById(R.id.puzzle_box_layout_2);
+        puzzleBoxLayout[3] = (LinearLayout) findViewById(R.id.puzzle_box_layout_3);
+
+        for( int row = 0;row < ROWS; row++) {
+            final LinearLayout currentLayout = puzzleBoxLayout[row];
+            for( int col = 0;col < COLS; col++) {
+                TextView currentView = (TextView) currentLayout.getChildAt(col);
+                currentView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            v.setBackgroundResource(R.drawable.normal_color);
+                        }
+                        return true;
+                    }
+                });
+            }
+        }
+    }
+
+    private void setOnTouchListenerOnLayout(){
+        final int ROWS = 4;
+        final int COLS = 5;
+        final int SIZE = 4;
+        final LinearLayout puzzleBoxLayout[] = new LinearLayout[SIZE];
+
+        puzzleBoxLayout[0] = (LinearLayout) findViewById(R.id.puzzle_box_layout_0);
+        puzzleBoxLayout[1] = (LinearLayout) findViewById(R.id.puzzle_box_layout_1);
+        puzzleBoxLayout[2] = (LinearLayout) findViewById(R.id.puzzle_box_layout_2);
+        puzzleBoxLayout[3] = (LinearLayout) findViewById(R.id.puzzle_box_layout_3);
+
+        for( int row = 0;row < ROWS; row++) {
+            final LinearLayout currentLayout =  puzzleBoxLayout[row];
+            currentLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+
+                    if (event.getAction() == MotionEvent.ACTION_UP) {
+                        for (int i = 0; i < currentLayout.getChildCount(); i++) {
+                            TextView view = (TextView) currentLayout.getChildAt(i);
+                            view.setBackgroundResource(R.drawable.normal_color);
+                        }
+                    }
+
+                    for (int i = 0; i < currentLayout.getChildCount(); i++) {
+                        TextView view = (TextView) currentLayout.getChildAt(i);
+                       /* if (!isPointWithin(x, y, view.getLeft(), view.getRight(), view.getTop(),
+                                view.getBottom())) {
+                            view.setBackgroundResource(R.drawable.normal_color);
+                        }*/
+                     /*  if(event.getAction() == MotionEvent.ACTION_DOWN){*/
+                           if (isPointWithin(x, y, view.getLeft(), view.getRight(), view.getTop(),
+                                   view.getBottom())) {
+                               view.setBackgroundResource(R.drawable.pressed_color);
+                           }
+                      /* }*/
+
+                    }
+                    return true;
+                }
+            });
+        }
+
+
+       /* for(int row = 0;row < ROWS; row++){
+            TextView view;
+            for(int col = 0; col < COLS;col++){
+                view = (TextView) puzzleBoxLayout[row].getChildAt(col);
+                view.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        *//*if(event.getAction() == MotionEvent.ACTION_MOVE){
+                            createCorrectWordTextView( "touchd" );
+                          *//**//*  createCorrectWordTextView( v.getId() + "" );*//**//*
+                            *//**//*v.setBackgroundResource(R.drawable.pressed_color);*//**//*
+                            // Do what you want
+                            return true;
+                        }*//*
+                        return true;
+                    }
+                });
+            }
+        }*/
+    }
+
+    private boolean isPointWithin(int x, int y, int x1, int x2, int y1, int y2) {
+        return (x <= x2 && x >= x1 && y <= y2 && y >= y1);
     }
 
     public void getSelectedLetter(View view){
