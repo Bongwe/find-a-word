7ae18cec07409427639aac5050c94f3bce2a1c82
mzwandile.mahlangu <mzwandile.mahlangu@entelect.co.za>
2017-06-23 23:19:01 +0200

    Added a toat for incorrect words. Set a onTouchListener on the main layer . Added a function to check when a puzzle box is touched

--- app/src/main/java/za/co/mzwandile/finlitapp_crossword_puzzle/MainActivity.java
+++ app/src/main/java/za/co/mzwandile/finlitapp_crossword_puzzle/MainActivity.java
@@ -1,68 +1,92 @@
 package za.co.mzwandile.finlitapp_crossword_puzzle;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.Console;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] correctWords = {"save","grow","earn"};
     private String constructedWord = "";
-    private int correctWordTextViewID = 0;
     private List<TextView> selectedTextViews = new LinkedList<>();
-    private List<TextView> puzzleBlocks = new LinkedList<>();
+    private List<TextView> selectedPuzzleBoxes = new LinkedList<>();
+    private LinearLayout mainLayout;
+    private GridLayout puzzleLayout;
+    //private List<TextView> puzzleBoxes = new LinkedList<>();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setOnTouchListenerOnLayout();
+
+        setTouchListenerOnMainLayout();
+        setTouchListenerOnPuzzleLayout();
     }
 
-    private void setOnTouchListenerOnLayout(){
-
-        final GridLayout gameGridLayout = (GridLayout) findViewById(R.id.puzzle_grid_layout);
-        gameGridLayout.setOnTouchListener(new View.OnTouchListener() {
+    private void setTouchListenerOnMainLayout(){
+        mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
+        mainLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-
-                for (int i = 0; i < gameGridLayout.getChildCount(); i++) {
-                    TextView view = (TextView) gameGridLayout.getChildAt(i);
-
-                    if (event.getAction() == MotionEvent.ACTION_UP) {
-                        view.setBackgroundResource(R.drawable.normal_color);
-                    }
-
-                    if(event.getAction() == MotionEvent.ACTION_MOVE) {
-                        if (isPointWithin(x, y, view.getLeft(), view.getRight(),
-                                view.getTop(), view.getBottom())) {
-                            view.setBackgroundResource(R.drawable.pressed_color);
-                        }
-                    }
-                }
+                highLightSelectedPuzzleBoxes( v,event);
                 return true;
             }
         });
+    }
+
+    private void setTouchListenerOnPuzzleLayout(){
+        puzzleLayout = (GridLayout) findViewById(R.id.puzzle_grid_layout);
+        puzzleLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction() == MotionEvent.ACTION_MOVE) {
+                    highLightSelectedPuzzleBoxes(v, event);
+                }
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    unHighLightDeselectedPuzzleBoxes(v, event);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void highLightSelectedPuzzleBoxes(View view, MotionEvent event){
+
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        for(int puzzleBox = 0; puzzleBox < puzzleLayout.getChildCount();puzzleBox++){
+            TextView currentPuzzleBox = (TextView) puzzleLayout.getChildAt(puzzleBox);
+            if (isPointWithin(x, y, currentPuzzleBox.getLeft(), currentPuzzleBox.getRight(),
+                    currentPuzzleBox.getTop(), currentPuzzleBox.getBottom())) {
+                selectedPuzzleBoxes.add(currentPuzzleBox);
+                currentPuzzleBox.setBackgroundResource(R.drawable.pressed_color);
+            }
+        }
+    }
+
+    private void unHighLightDeselectedPuzzleBoxes(View view, MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_UP) {
+            for(int puzzleBox = 0; puzzleBox < selectedPuzzleBoxes.size();puzzleBox++){
+                TextView selectedPuzzleBox = selectedPuzzleBoxes.get(puzzleBox);
+                selectedPuzzleBox.setBackgroundResource(R.drawable.normal_color);
+            }
+        }
     }
 
     private boolean isPointWithin(int x, int y, int x1, int x2, int y1, int y2) {
@@ -70,14 +94,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getSelectedLetter(View view){
-        Log.v("getSelectedLetter","pressed!");
         String clickedButtonText;
         TextView clickedView;
         StringBuilder stringBuilder;
 
         clickedView = (TextView)view;
         clickedButtonText = clickedView.getText().toString();
-       /* clickedView.setBackgroundResource(R.drawable.pressed_color);*/
         selectedTextViews.add(clickedView);
         stringBuilder = new StringBuilder(constructedWord);
 
@@ -86,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkIfConstructedWordIsCorrect(View view){
-        String message = " is incorrect!";
+        String message = constructedWord +" is incorrect!";
         changeBackgroundColor();
         for (int i = 0; i < correctWords.length;i++){
             if (correctWords[i].toUpperCase().equals(constructedWord)) {
@@ -95,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         }
-        createInCorrectWordTextView(constructedWord);
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
         constructedWord = "";
     }
 
@@ -125,25 +148,6 @@ public class MainActivity extends AppCompatActivity {
         newTextView.setPadding(padding,padding,padding,padding);
         newTextView.setLayoutParams(params);
         newTextView.setBackgroundColor(Color.parseColor("#67BC8C"));
-        layout.addView(newTextView);
-    }
-
-    private void createInCorrectWordTextView(String word){
-        TextView newTextView = new TextView(this);
-        LinearLayout layout = (LinearLayout)findViewById(R.id.correctWordsLayout);
-        int padding = 7;
-        int margin = 5;
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
-                (ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-        params.setMargins(margin,margin,margin,margin);
-
-        newTextView.setText(word);
-        newTextView.setTextSize(16);
-        newTextView.setTextColor(Color.parseColor("#000000"));
-        newTextView.setPadding(padding,padding,padding,padding);
-        newTextView.setLayoutParams(params);
-        newTextView.setBackgroundColor(Color.parseColor("#BC6E67"));
         layout.addView(newTextView);
     }
 }
